ae6e8fb866c78f95d38ede171192de0a58daa964
Isidro Sosa <isidro.sosa.p@gmail.com>
2017-08-19 15:38:33 -0500

    Add router to home in menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { PortfolioComponent, AboutComponent, ItemComponent } from './components/
 
 
 const app_routes: Routes = [
-  { path: '', component: PortfolioComponent },
+  { path: 'home', component: PortfolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'product', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
 export const appRouting = RouterModule.forRoot(app_routes, { useHash: true });
