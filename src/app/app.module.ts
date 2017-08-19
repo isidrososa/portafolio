98756020c5c703a48e6c08850a015ef6674f1b16
Isidro Sosa <isidro.sosa.p@gmail.com>
2017-08-19 14:44:11 -0500

    Copy header, footer and portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
