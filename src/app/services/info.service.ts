7b0dd5d4c8b9276b4f4a6244626ef3857c9ea4c0
Isidro Sosa <isidro.sosa.p@gmail.com>
2017-08-19 19:03:34 -0500

    Add services load  team

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -4,15 +4,30 @@ import { Http } from '@angular/http';
 @Injectable()
 export class InfoService {
   info: any = {};
-  loading: boolean = false;
+  loadingData: boolean = false;
+  loadingTeam: boolean = false;
+  team: any = [];
 
   constructor(public http: Http) {
+    this.loadInfo();
+    this.loadTeam();
+  }
+
+  loadInfo(): void {
     this.http.get('assets/data/info.page.json')
       .subscribe(data => {
         this.info = data.json();
-        console.log(this.info);
-        this.loading = true;
+        this.loadingData = true;
       });
   }
 
+  loadTeam(): void {
+    let url: string = "https://notificaciones-12221.firebaseio.com/team.json";
+    this.http.get(url)
+      .subscribe(data => {
+        console.log(data.json());
+        this.team = data.json();
+        this.loadingTeam = true;
+      });
+  }
 }
