594b9c1b49497a2c12704797264a1f44384e923c
Isidro Sosa <isidro.sosa.p@gmail.com>
2017-08-19 19:17:53 -0500

    Load service team to show info in about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { InfoService } from 'app/services/info.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styles: []
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class AboutComponent {
+  constructor(public infoService: InfoService) { }
 }
